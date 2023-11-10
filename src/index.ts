30df3ce8cbd5af952380670ccbbaaf94c266b15e
andrianggoro21 <andri.anggoro21@gmail.com>
2023-11-10 15:03:04 +0700

    init backend

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({
+    path: path.resolve(__dirname, "../.env")
+});
+
+const port: number = Number(process.env.PORT) || 8000; 
+const app = express();
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+})
+
