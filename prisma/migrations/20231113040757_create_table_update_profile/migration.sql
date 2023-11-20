b6e8f1919a00adddc6bb86d2905da981460b15b7
Asrar Abuchaer <asrarabuchaer@gmail.com>
2023-11-20 14:59:42 +0700

    transaction query

--- prisma/migrations/20231113040757_create_table_update_profile/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `Update_Profile` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `image` VARCHAR(1000) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
